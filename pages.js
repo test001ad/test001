d2385b0b93f2ae17a7ddeca60930853954fa9c2b
argoub <hp-2012@hotmail.fr>
2019-03-17 06:49:21 +0000

    Update pages.js

--- pages.js
+++ pages.js
@@ -8,7 +8,7 @@ var PAGES = [
 		title: "Don't forget your daily free coins",
 		subtitle: "We miss you, come back and play",
 		cta: "Play Now",
-		imageurl: "<URL of image to show in message>",
+		imageurl: "http://cdn.akamai.steamstatic.com/steam/apps/771710/ss_9c45538e93098a30e6974480a4d83323c2f0e236.jpg",
 		payload: null,
 		pat: "EAAEyFHJtNI8BAK6CO314NhIemddClVzGuOgG6a3QBAqhh57EAKwdibXHVhnKKesvn0n0MJ4ovwPIBo7ZA8PwNbnQcTTcZCthmLZC5xdkPHbjNN08SHrpZAwm3rvnotISZBeDWAH8UqWecOiz5VCnxJAJdXxr0kWHOmLJdLVsxuH6UXX0uEyQz",
 	},
