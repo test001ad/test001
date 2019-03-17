56c4ddc2d8f4c87f768be32156512dbd70505666
argoub <hp-2012@hotmail.fr>
2019-03-17 06:01:34 +0000

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,16 +1,16 @@
-var VERIFY_TOKEN = "<Your custom verification code here>";
+var VERIFY_TOKEN = "mygame";
 var PAGES = [
 	// Add your pages here, change key for each one, I use numberical values starting at 0
 	{
-		id: "<Your page ID>",
-		name: "<Game Name>",
+		id: "2324247294255216",
+		name: "My game",
 		key: "0",
 		title: "Don't forget your daily free coins",
 		subtitle: "We miss you, come back and play",
 		cta: "Play Now",
 		imageurl: "<URL of image to show in message>",
 		payload: null,
-		pat: "<Enter tour Page Access Token here>",
+		pat: "EAAEyFHJtNI8BAK6CO314NhIemddClVzGuOgG6a3QBAqhh57EAKwdibXHVhnKKesvn0n0MJ4ovwPIBo7ZA8PwNbnQcTTcZCthmLZC5xdkPHbjNN08SHrpZAwm3rvnotISZBeDWAH8UqWecOiz5VCnxJAJdXxr0kWHOmLJdLVsxuH6UXX0uEyQz",
 	},
 ];
 
