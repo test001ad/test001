f93a1403eac6a7b63a81783571b32840b133522e
zakaria kasmi <zakaria_kasmi@hotmail.fr>
2019-03-19 10:35:49 +0100

    update bot.js sendregularity first 2 digits replaced with 0

--- bot.js
+++ bot.js
@@ -172,8 +172,8 @@ function AddPlayer(sender_id, player_id, context_id, game) {
 
 
 var SendRegularity = [
-	1,	// Day 1
-	1,	// Day 2
+	0,	// Day 1
+	0,	// Day 2
 	2,	// Day 4
 	2,	// Day 6
 	1,	// Day 7
