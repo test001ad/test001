47f88327f4d0a4f48b33ba4d70a2503d0cdda22c
zakaria kasmi <zakaria_kasmi@hotmail.fr>
2019-03-19 07:46:34 +0100

    Change Bot.js added console

--- bot.js
+++ bot.js
@@ -6,73 +6,61 @@ var cors = require("cors");
 var redis = require("redis");
 
 var red = redis.createClient();
-red.on("error", (err) =>
-{
-	//console.log("Redis error: " + err);
+red.on("error", (err) => {
+    console.log("Redis error: " + err);
 });
 
 app.set("port", (process.env.PORT || 8000));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(app.get("port"), function()
-{
+app.listen(app.get("port"), function () {
 });
 
-app.get("/bot", function(request, response)
-{
-	if (request.query["hub.mode"] === "subscribe" && request.query["hub.verify_token"] === pages.GetVertifyToken())
-	{
-		response.status(200).send(request.query["hub.challenge"]);
-	}
-	else
-	{
-		response.sendStatus(403);          
-	}  
+app.get("/bot", function (request, response) {
+    if (request.query["hub.verify_token"] === pages.GetVertifyToken()) {
+        response.status(200).send(request.query["hub.challenge"]);
+    }
+    else {
+        response.sendStatus(403);
+    }
 });
 
-app.post("/bot", function(request, response)
-{
-	var data = request.body;
-	 console.log('received bot webhook');
-	if (data.object === "page" && data.entry !== undefined)
-	{
-		data.entry.forEach(function(entry)
-		{
-			if (entry.messaging !== undefined)
-			{
-				var page_id = entry.id;
-				// Iterate over each messaging event
-				entry.messaging.forEach(function(event)
-				{
-					if (event.message)
-					{
-						HandleMessage(event);
-					}
-					else if (event.game_play)
-					{
-						HandleGameplay(event, pages.GetGame(page_id));
-					}
-					else
-					{
-						// console.log("Webhook received unknown event: ", event);
-					}
-				});
-			}
-		});
-	}
-	response.sendStatus(200);
+app.post("/bot", function (request, response) {
+    var data = request.body;
+    console.log('1 received bot webhook');
+    if (data.object === "page" && data.entry !== undefined) {
+        console.log('2 Data object Page ');
+        data.entry.forEach(function (entry) {
+            if (entry.messaging !== undefined) {
+                var page_id = entry.id;
+                // Iterate over each messaging event
+                entry.messaging.forEach(function (event) {
+                    if (event.message) {
+                        HandleMessage(event);
+                        console.log("Someone sent a message ... ");
+                    }
+                    else if (event.game_play) {
+                        console.log("Someone Plays the Game  ... ");
+                        HandleGameplay(event, pages.GetGame(page_id));
+                    }
+                    else {
+                        console.log("Webhook received unknown event: ");
+                    }
+                });
+            }
+        });
+    }
+    response.sendStatus(200);
 });
 
-function HandleMessage(event)
-{
+function HandleMessage(event) {
 }
 
-function HandleGameplay(event, game)
-{
-	var sender_id = event.sender.id; 
-	var player_id = event.game_play.player_id; 
-	var context_id = event.game_play.context_id;
+function HandleGameplay(event, game) {
+    var sender_id = event.sender.id;
+    var player_id = event.game_play.player_id;
+    var context_id = event.game_play.context_id;
 
 	/*if (event.game_play.payload)
 	{
@@ -83,34 +71,32 @@ function HandleGameplay(event, game)
 		var payload = JSON.parse(event.game_play.payload);
 	}*/
 
-	AddPlayer(sender_id, player_id, context_id, game);
+    AddPlayer(sender_id, player_id, context_id, game);
 }
 
-function AddPlayer(sender_id, player_id, context_id, game)
-{
-	var now = Date.now();
-	var key = game.key + ":" + player_id;
+function AddPlayer(sender_id, player_id, context_id, game) {
+    var now = Date.now();
+    var key = game.key + ":" + player_id;
 
-	red.exists(key, function(err, exists)
-	{
-		if (!exists)
-		{
-			// Player does not exist so add them
-			// pid - Page scoped player id
-			// cid - Context ID
-			// tsm - Total sent messages since player last played
-			// lt - Last time a message was sent (resets each timea message is sent or the player plays the game)
-			if (context_id !== undefined)
-				red.hmset(key, "pid", sender_id, "cid", context_id, "tsm", "0", "lt", now);
-			else
-				red.hmset(key, "pid", sender_id, "tsm", "0", "lt", now);
-		}
-		else
-		{
-			// Player has come back so reset send
-			red.hmset(key, "tsm", "0", "lt", now);
-		}
-	});
+    red.exists(key, function (err, exists) {
+        if (!exists) {
+            console.log("  Adding the Player to  Redis... ");
+            // Player does not exist so add them
+            // pid - Page scoped player id
+            // cid - Context ID
+            // tsm - Total sent messages since player last played
+            // lt - Last time a message was sent (resets each timea message is sent or the player plays the game)
+            if (context_id !== undefined)
+                red.hmset(key, "pid", sender_id, "cid", context_id, "tsm", "0", "lt", now);
+            else
+                red.hmset(key, "pid", sender_id, "tsm", "0", "lt", now);
+
+
+        }
+        else {
+            // Player has come back so reset send
+            red.hmset(key, "tsm", "0", "lt", now);
+            console.log("Reset the player  ... ");
+        }
+    });
 }
-
-
