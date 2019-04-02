bf16875c216dd5124f43138e5bb036d80212e772
zakaria kasmi <zakaria_kasmi@hotmail.fr>
2019-04-02 08:10:25 +0100

    reverse 2 games order

--- pages.js
+++ pages.js
@@ -1,28 +1,29 @@
 var VERIFY_TOKEN = "mygame";
 var PAGES = [
 	// Add your pages here, change key for each one, I use numberical values starting at 0
-	{
-		id: "2324247294255216",
-		name: "My game",
-		key: "0",
-		title: "Don't forget your daily free coins",
-		subtitle: "We miss you, come back and play",
-		cta: "Play Now",
-		imageurl: "http://cdn.akamai.steamstatic.com/steam/apps/771710/ss_9c45538e93098a30e6974480a4d83323c2f0e236.jpg",
-		payload: null,
-		pat: "EAAEyFHJtNI8BAK6CO314NhIemddClVzGuOgG6a3QBAqhh57EAKwdibXHVhnKKesvn0n0MJ4ovwPIBo7ZA8PwNbnQcTTcZCthmLZC5xdkPHbjNN08SHrpZAwm3rvnotISZBeDWAH8UqWecOiz5VCnxJAJdXxr0kWHOmLJdLVsxuH6UXX0uEyQz",
-    },
     {
         id: "2203277176590538",
         name: "Candy Crush",
-        key: "1",
+        key: "0",
         title: "It's a brand new day, let's continue our adventure!",
         subtitle: "Your Friends are playing...",
         cta: "Play Now",
         imageurl: "https://pmcvariety.files.wordpress.com/2018/08/candy-crush-saga.jpg?w=1000&h=563&crop=1",
         payload: null,
         pat: "EAAfT3nFz0MoBAJaBX9vN95xVA3ZCGwZBWUbJ1IvWlRqL4ywAfEGkIfsPJo9dO50xyicapEFEZAj1ZAkOE7yWGs8VFrZBZBTYHzcPYh83sunLRZAzLUs0NFSqySNoDIoR2FaruMd4wP4yxvS69X8kXQceAJrXQmsaM5CCaXKPW2TPpBUyYuLeR1U",
+    },
+	{
+		id: "2324247294255216",
+		name: "My game",
+		key: "1",
+		title: "Don't forget your daily free coins",
+		subtitle: "We miss you, come back and play",
+		cta: "Play Now",
+		imageurl: "http://cdn.akamai.steamstatic.com/steam/apps/771710/ss_9c45538e93098a30e6974480a4d83323c2f0e236.jpg",
+		payload: null,
+		pat: "EAAEyFHJtNI8BAK6CO314NhIemddClVzGuOgG6a3QBAqhh57EAKwdibXHVhnKKesvn0n0MJ4ovwPIBo7ZA8PwNbnQcTTcZCthmLZC5xdkPHbjNN08SHrpZAwm3rvnotISZBeDWAH8UqWecOiz5VCnxJAJdXxr0kWHOmLJdLVsxuH6UXX0uEyQz",
     }
+ 
 ];
 
 function GetGame(page_id)
