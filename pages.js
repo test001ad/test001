28e72d2fb85d87d46b70275e75cf52b6505b3bf6
zakaria kasmi <zakaria_kasmi@hotmail.fr>
2019-05-04 23:17:21 +0100

    adding toy blast

--- pages.js
+++ pages.js
@@ -22,7 +22,22 @@ var PAGES = [
 		imageurl: "http://cdn.akamai.steamstatic.com/steam/apps/771710/ss_9c45538e93098a30e6974480a4d83323c2f0e236.jpg",
 		payload: null,
 		pat: "EAAEyFHJtNI8BAK6CO314NhIemddClVzGuOgG6a3QBAqhh57EAKwdibXHVhnKKesvn0n0MJ4ovwPIBo7ZA8PwNbnQcTTcZCthmLZC5xdkPHbjNN08SHrpZAwm3rvnotISZBeDWAH8UqWecOiz5VCnxJAJdXxr0kWHOmLJdLVsxuH6UXX0uEyQz",
+    }, {
+        id: "329459777693439",
+        name: "Toy Blast",
+        key: "1",
+        title: "Don't forget your daily free coins",
+        subtitle: "We miss you, come back and play",
+        cta: "Play Now",
+        imageurl: "https://i.imgur.com/SETRPcn.png",
+        payload: null,
+        pat: "EAAeZB84SlSooBAJp8ZBvlxi65AjRzDH79ZBIZBIKEb9VW54OiNUqgNuDZBhsubZBf2HiiTeTWAoQsXeDaeWKegPpQSPrBcpZAgebggd36ASlzFc12ZBPN5Lu5A6aExCPxhCABQgd8PlJrg5sq7VDaImn33E2y7uDk5xZBcqnoRG89ZAKCoqt5uCLPk",
     }
+
+
+
+
+
  
 ];
 
